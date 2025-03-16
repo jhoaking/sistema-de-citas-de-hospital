19698bc0e3cb1aa79874b082860e4bbd6ce13691
jhoaking <peredoroj@gmail.com>
2025-03-16 12:41:14 -0400

    cree la logica para los tokens y una parte en sus controller

---
+++ src/controller/jwt.controller.js
@@ -0,0 +1,54 @@
+import { validarUsuario } from "../schema/esquemas";
+import { SALT_ROUNDS } from "../config";
+import jwt from 'jsonwebtoken'
+
+export class verificacioController {
+    constructor ({modelJwt}){
+        this.modelJwt = modelJwt;
+    }
+
+    register = async(req,res) =>{
+        try {
+            const vali = validarUsuario(req.body);
+            if(!vali.success){
+                return res.status(400).json({message: 'error al validar datos del register'})
+            }
+            const user = await this.modelJwt.registerUser(vali.data);
+
+            res.status(200).send({user});
+        } catch (error) {
+            res.status(500).json({message : 'error al registrar user', error: error.message});        
+        }
+    }
+
+
+    login = async (req,res) =>{
+        try {
+            const vali = validarUsuario(req.body);
+            if(!vali.success){
+                return res.status(400).json({message: 'error al validar datos del login'})
+            }
+
+            const user = await this.modelJwt.loginUser(vali.data);
+
+            if(!result){ throw new Error('no se encontro el usuario')}
+
+            const token = jwt.sign({
+             id : user.id , nombre: user.nombre, apellido : user.apellido },
+             SALT_ROUNDS,
+            { expiresIn: '48h'})
+
+            res
+            .cookie('access_token',token, {
+                httpOnly: true, 
+                sameSite : 'strict', 
+                secure : process.env.NODE_ENV === 'production',
+                maxAge: 1000 * 60 * 60 * 48
+            }) 
+
+            res.send({user,token})
+        } catch (error) {
+            res.status(500).json({message : 'error al logear user', error: error.message});
+        }
+    }
+} 
--- src/model/validacionjwt.model.js
+++ src/model/validacionjwt.model.js
@@ -1,15 +1,15 @@
 import {connection} from '../db.js';
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { SALT_ROUNDS } from '../config.js';
 
 export class modelJwt {
-    static register = async ({nombre, apellido,email,pasword}) =>{
+    static registerUser = async ({nombre, apellido,email,password}) =>{
        try {
         const [rows] = await connection.query('SELECT * FROM users WHERE email = ?',[email]);
         if(rows.length > 0) {throw new Error('el usuario ya existe')}
 
-        const hashedPassword  = await bcrypt.hash(pasword, Number(SALT_ROUNDS));
+        const hashedPassword  = await bcrypt.hash(password, Number(SALT_ROUNDS));
+
         await connection.query('INSERT INTO users(nombre, apellido,email,pasword) VALUES (?,?,?,?)',[nombre,apellido,email,hashedPassword]);
 
         const [newUser] = await connection.query
@@ -21,17 +21,18 @@ export class modelJwt {
        }
     }
 
-    static login = async ({email,password}) =>{
+    static loginUser = async ({email,password}) =>{
         try {
             const [result] = await connection.query
-            ('SELECT BIN_TO_UUID(user_id), nombre,apellido,email WHERE email = ?',[email])
-            if(result.length > 0) {throw new Error('ya fuiste logeado')}
+            ('SELECT BIN_TO_UUID(user_id), nombre,apellido,email FROM users WHERE email = ?',[email])
+            if(result.length === 0) {throw new Error('datos invalidos')}
 
             const user = result[0];
 
-            const compararPassword = await bcrypt.compare(password,user.password);
+            const compararPassword = await bcrypt.compare(password, user.password);
             if(!compararPassword){throw new Error('contraseña invalida')}
 
+            delete user.password;
             return user;
         } catch (error) {
             console.error('error al registrar usuarios - modelo',error);
